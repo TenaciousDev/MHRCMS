9c8fc067d7e2459cf67c87bbdac7f5a9c9f62eb4
A. S. Metcalf <70526588+asmetcalf@users.noreply.github.com>
2020-11-13 08:28:50 -0500

    Add project files.

---
+++ MHRCMS.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MHRCMS.WebMVC.MvcApplication" Language="C#" %>
---
+++ MHRCMS.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MHRCMS.WebMVC.Startup))]
+namespace MHRCMS.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
