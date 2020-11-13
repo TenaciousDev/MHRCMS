2cd2bb5483d5faa2414939bba0b63246c43f4b8c
A. S. Metcalf <70526588+asmetcalf@users.noreply.github.com>
2020-11-13 11:01:49 -0500

    began building out Data layer

--- MHRCMS.Data/UnitInfo.cs
+++ MHRCMS.Data/UnitInfo.cs
@@ -8,5 +8,17 @@ namespace MHRCMS.Data
 {
     public class UnitInfo
     {
+        public int Id { get; set; }
+        public int PersonnelId { get; set; }
+        public int PlatoonId { get; set; }
+        public int SquadId { get; set; }
+        public int TeamId { get; set; }
+        public string Role { get; set; }
+        public DateTimeOffset Arrived { get; set; }
+        public string DutyStatus { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid ModifiedLast { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
